58e24e1dd0be2ce9726e4f631dc85e4aa530ab90
Wiiisal <wissalbensai011@gmail.com>
2022-11-15 21:33:36 +0100

    adding video

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pokimon
+npm install boutsrap
---
+++ src/Video.js
@@ -0,0 +1,6 @@
+import ReactPlayer from 'react-player/youtube'
+import React from 'react'
+var Video=()=>{
+return (<ReactPlayer class="position-absolute top-50 end-0 translate-middle-y" url='https://www.youtube.com/watch?v=NBElzosmpYI' />)
+}
+export default Video;
